a560cf39253c3ba95be409434bd083aacc4a36cd
佳斌 李 <l3234216896@gmail.com>
2025-04-02 01:18:37 +0800

    1

---
+++ 25_4_2/test.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<math.h>
+//int main()
+//{
+//	int a;
+//	int b;
+//	for (a = 100; a <= 200; a++)
+//	{
+//		for (b = 2; b <= sqrt(a); b++) {
+//			if (a % b != 0) {
+//				printf("%d\n", a);
+//
+//			}
+//			else
+//			{
+//				break;
+//			}
+//		}
+//	}
+//	return 0;
+//}
+
+int main() {
+	int a;
+	int b;
+	int c;
+	int d;
+	scanf_s("%d%d" ,&a, &b);
+	d = a > b ? a:b;
+	scanf_s("%d", &c);
+	d = d > c ? d : c;
+	printf("%d", d);
+	return 0;
+}
+
+//int main()
+//{
+//	int a=0;
+//	int b;
+//	scanf_s("%d", a);
+//	if (a < 1000) {
+//		b = sqrt(a);
+//		printf("%d", b);
+//	}
+//	else {
+//		printf("请重新输入小于1000的数字");
+//	}
+//	return 0;
+//}
